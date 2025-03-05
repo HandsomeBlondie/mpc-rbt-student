ba946c9ee7df49dc2fc0af000062cc9277fc113f
HandsomeBlondie <240349@vut.cz>
2025-03-05 15:06:55 +0100

    Update Sender.hpp
    oprava

--- include/mpc-rbt-solution/Sender.hpp
+++ include/mpc-rbt-solution/Sender.hpp
@@ -1,40 +1,40 @@
 #ifndef COMMUNICATION_EXAMPLE_SENDER_H
 #define COMMUNICATION_EXAMPLE_SENDER_H
 
-// clang-format off
-#include <chrono>
-
 #include <mpc-rbt-solution/util/Socket.hpp>
 #include <mpc-rbt-solution/util/Utils.hpp>
+
 // clang-format on
 
 namespace Sender
 {
-class Node : public Socket::UDP
-{
-public:
-  explicit Node(const Utils::Config::Sender & senderConfig)
-  : Socket::UDP(senderConfig.localPort),
-    config(senderConfig),
-    timer_period(config.sendingPeriodMillis)
-  {
-    socket = std::make_unique<Socket>(localPort);
-    callback = [this]() { this->onDataTimerTick(); };
-  }
+    class Node : public Socket::UDP
+    {
+    public:
+        explicit Node(const Utils::Config::Sender & senderConfig)
+            : Socket::UDP(senderConfig.LocalPort),
+              config(senderConfig),
+              timer_period(config.sendingPeriodMillis)
+        {
+            create();
+            configure();
+            bind();
+            callback = [this]() { onDataTimerTick(); };
+        }
 
-  void run();
+        void run();
 
-private:
-  void onDataTimerTick();
+    private:
+        void onDataTimerTick();
 
-  Utils::Message data{};
+        Utils::Message data{};
+        Utils::Config::Sender config;
+        std::chrono::milliseconds timer_period;
+        std::chrono::time_point<std::chrono::steady_clock> timer_tick = std::chrono::steady_clock::now();
+        std::function<void()> callback;
+        rclcpp::Logger logger{rclcpp::get_logger("sender")};
+    };
 
-  Utils::Config::Sender config;
-  std::chrono::milliseconds timer_period;
-  std::chrono::time_point<std::chrono::steady_clock> timer_tick = std::chrono::steady_clock::now();
-  std::function<void()> callback;
-  rclcpp::Logger logger{rclcpp::get_logger("sender")};
-};
-}  // namespace Sender
+} // namespace Sender
 
-#endif  // COMMUNICATION_EXAMPLE_SENDER_H
+#endif // COMMUNICATION_EXAMPLE_SENDER_H
