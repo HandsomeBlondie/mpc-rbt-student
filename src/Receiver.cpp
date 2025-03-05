8ba3183ccd1ac515ac123ca59ca8078ac1f4d876
HandsomeBlondie <240349@vut.cz>
2025-03-05 15:03:43 +0100

    Update Receiver.cpp
    oprava

--- src/Receiver.cpp
+++ src/Receiver.cpp
@@ -1,34 +1,25 @@
-#include <mpc-rbt-solution/Receiver.hpp>
+#include "Receiver.hpp"
 
-void Receiver::Node::run()
+void Receiver::run()
 {
-  while (errno != EINTR) {
-    RCLCPP_INFO(logger, "Waiting for data ...");
-    Socket::IPFrame frame{};
-    if (receive(frame)) {
-      RCLCPP_INFO(logger, "Received data from host: '%s:%d'", frame.address.c_str(), frame.port);
-
-      callback(frame);
-
-    } else {
-      RCLCPP_WARN(logger, "Failed to receive data.");
+    while (errno != EINTR)
+    {
+        RCLCPP_INFO(logger, "Waiting for data...");
+        Socket::IPFrame frame;
+        if (receive(frame))
+        {
+            RCLCPP_INFO(logger, "Received data from host: %s:%d", frame.address.c_str(), frame.port);
+            callback(frame);
+        }
+        else
+        {
+            RCLCPP_WARN(logger, "Failed to receive data.");
+        }
     }
-  }
 }
 
-void Receiver::Node::onDataReceived(const Socket::IPFrame & frame)
+void Receiver::onDataReceived(const Socket::IPFrame & frame)
 {
-  try {
-    // Deserializace přijatých dat
-    json j = json::parse(frame.data);
-    data = Utils::jsonToMessage(j);
-
-    // Logování přijatých dat
-    RCLCPP_INFO(logger, "Received data:");
-    RCLCPP_INFO(logger, "\t x: %.2f\n\t y: %.2f\n\t z: %.2f\n\t timestamp: %ld", data.x, data.y, data.z, data.timestamp);
-
-  } catch (const std::exception & e) {
-    RCLCPP_ERROR(logger, "Failed to parse received data: %s", e.what());
-  }
+    Utils::Message::deserialize(frame.data);
+    RCLCPP_INFO(logger, "\nTimestamp: %ld", data.timestamp);
 }
-
