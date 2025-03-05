8450ba7b25c3cdd10a3cf3342055e8a9cc716521
HandsomeBlondie <240349@vut.cz>
2025-03-05 14:07:20 +0100

    Update Sender.cpp
    Oprava

--- src/Sender.cpp
+++ src/Sender.cpp
@@ -12,16 +12,29 @@ void Sender::Node::run()
 
 void Sender::Node::onDataTimerTick()
 {
-  UNIMPLEMENTED(__PRETTY_FUNCTION__);
+  // Generování dat
+  data.x += 1.0;
+  data.y += 2.0;
+  data.z += 3.0;
 
-  data.timestamp =
-    static_cast<uint64_t>(std::chrono::system_clock::now().time_since_epoch().count());
+  data.timestamp = static_cast<uint64_t>(std::chrono::system_clock::now().time_since_epoch().count());
 
+  // Serializace do JSON
+  json j = Utils::messageToJson(data);
+  std::string serializedData = j.dump();
+
+  // Nastavení cílového hosta
   Socket::IPFrame frame{
     .port = config.remotePort,
     .address = config.remoteAddress,
+    .data = serializedData
   };
-  RCLCPP_INFO(logger, "Sending data to host: '%s:%d'", frame.address.c_str(), frame.port);
 
-  RCLCPP_INFO(logger, "\n\tstamp: %ld", data.timestamp);
+  // Logování odesílání
+  RCLCPP_INFO(logger, "Sending data to host: '%s:%d'", frame.address.c_str(), frame.port);
+  RCLCPP_INFO(logger, "\t x: %.2f\n\t y: %.2f\n\t z: %.2f\n\t timestamp: %ld", data.x, data.y, data.z, data.timestamp);
+
+  // Odeslání
+  send(frame);
 }
+
